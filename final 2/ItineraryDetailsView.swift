3ace5fb28d6254835e6c6b11da78b1448031bb4e
James Toh <jtyflol@gmail.com>
2023-11-24 15:10:18 +0800

    remove metadata

--- final 2/ItineraryDetailsView.swift
+++ final 2/ItineraryDetailsView.swift
@@ -11,18 +11,20 @@ struct itineraryDetailsView: View {
     @Binding var itinerary: Itinerary
     @ObservedObject var itineraryManager: ItineraryManager
     @State var additemtransit = false
-
+    
     var body: some View {
         VStack{
+            DatePicker("Start:", selection: $itinerary.startdate)
+            DatePicker("End:", selection: $itinerary.enddate)
             Spacer()
         }
         .navigationTitle(itinerary.country)
         .navigationBarItems(trailing:
-            NavigationLink(destination: addItemView(itineraryManager: itineraryManager)) {
-                Image(systemName: "Add Item")
-                    .resizable()
-                    .frame(width: 18, height: 18)
-            }
+                                NavigationLink(destination: addItemView(itineraryManager: itineraryManager)) {
+            Image(systemName: "Add Item")
+                .resizable()
+                .frame(width: 18, height: 18)
+        }
         )
         .toolbar{
             ToolbarItem{
